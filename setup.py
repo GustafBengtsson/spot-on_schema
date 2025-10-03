2df6765830ede9b81930607b6c887fc0f9cb5aa8
gustafbe <gustaf.bengtsson@ri.se>
2025-10-03 12:04:48 +0200

    Created framework for spot-on_schema

--- setup.py
+++ setup.py
@@ -1,13 +1,24 @@
 from setuptools import setup, find_packages
 
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
 setup(
-    name="spot_on_schema",
-    version="1.0.0",
+    name="spot-on_schema",
+    version="0.0.1",
     description="Shared SQLAlchemy schema models for spot-on projects",
     author="Gustaf B",
+    author_email="gustaf.bengtsson@rise.se",
     packages=find_packages(),
     install_requires=[
         "SQLAlchemy>=1.4"
+    ],
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Programming Language :: Python :: 3.8",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
     ],
     python_requires=">=3.8",
 )
